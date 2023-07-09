f6c2afc1c2b6239af2936868a0b226cb175b36bf
lkhzz <leta6603@gmail.com>
2023-07-09 13:28:34 +0900

    Initial commit

---
+++ src/main/java/com/example/loginwithredis/LoginWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginwithredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginWithRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loginwithredis/LoginWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginwithredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
