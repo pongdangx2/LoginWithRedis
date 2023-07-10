4f137725fd961a12417c59e418ae74d2fdfd7d0f
lkhzz <leta6603@gmail.com>
2023-07-10 21:57:04 +0900

    완성

--- src/main/java/com/example/loginwithredis/common/ErrorCode.java
+++ src/main/java/com/example/loginwithredis/common/ErrorCode.java
@@ -7,12 +7,11 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-      SUCCESS("00", "SUCCESS")
+      SUCCESS("00", "성공")
     , COMMON_ERROR("01", "처리중 에러가 발생하였습니다.")
     , DUPLICATE_ID("10", "중복된 ID 입니다.")
     , NONEXISTENT_ID("11", "존재하지 않는 ID 입니다.")
-    , TOO_SHORT_PASSWORD("20", "패스워드는 10자리이상이어야 합니다.")
-    , PASSWORD_DO_NOT_MATCH("21", "패스워드가 일치하지 않습니다.");
+    , PASSWORD_DO_NOT_MATCH("20", "패스워드가 일치하지 않습니다.");
 
     private String code;
     private String message;
